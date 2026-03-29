d021a99258107d2b45ef6e6bc70eb1ce92b8cdb5
maksluczak <maksluczak99@gmail.com>
2026-03-29 22:24:35 +0200

    enable cors

--- src/Services/ApiGateway/Program.cs
+++ src/Services/ApiGateway/Program.cs
@@ -8,8 +8,19 @@ builder.Services.AddLogging(logging =>
     logging.AddConsole();
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("DefaultPolicy");
 app.MapReverseProxy();
 
 app.Run();

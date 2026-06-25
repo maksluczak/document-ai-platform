ed261d1f592a4579394d1b68f8104806ec8c774c
maksluczak <maksluczak99@gmail.com>
2026-06-25 17:13:29 +0200

    file validator test

---
+++ server/Tests/UploadService.Tests/FileValidatorTests.cs
@@ -0,0 +1,30 @@
+namespace UploadService.Tests;
+
+using Xunit;
+using FluentAssertions;
+using UploadService.Validation;
+
+public class UnitTest1
+{
+    [Fact]
+    public void IsValidExtension_WhenFileIsPdf_ShouldReturnTrue()
+    {
+        var validator = new FileValidator();
+        var validFileName = "filename.pdf";
+
+        var result = validator.IsValidExtension(validFileName);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsValidExtension_WhenFileIsPng_ShouldReturnFalse()
+    {
+        var validator = new FileValidator();
+        var validFileName = "filename.png";
+
+        var result = validator.IsValidExtension(validFileName);
+
+        result.Should().BeFalse();
+    }
+}
